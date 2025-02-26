f4ddf69bce1950ddb54c869371d87b29cd6ac014
Kirti <kirtijhala1110@gmail.com>
2025-02-27 00:35:21 +0530

    demo folder created that will hold our entire Spring boot project

---
+++ demo/src/main/java/org/projectwallah/demo/ClassFirst.java
@@ -0,0 +1,11 @@
+package org.projectwallah.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassFirst {
+
+    public void show(){
+        System.out.println("Hii Kirti, I'm ClassFirst...");
+    }
+}
---
+++ demo/src/test/java/org/projectwallah/demo/KirtiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projectwallah.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
